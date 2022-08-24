0817033733c51316f86c3e74e4ca91bdca7ad3f3
Jamal The Atlantean <jamaltheatlantean@gmail.com>
2022-08-24 23:10:07 +0100

    Update 01-deploy.js

--- deploy/01-deploy.js
+++ deploy/01-deploy.js
@@ -12,7 +12,7 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
 
   let ethUsdPriceFeedAddress;
   if (developmentChains.includes(network.name)) {
-    const ethUsdAggregator = await deployments.get("MockV3Aggregator");
+    const ethUsdAggregator = await deployments.getContractAt("MockV3Aggregator");
     ethUsdPriceFeedAddress = ethUsdAggregator.address;
   } else {
     ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"];
