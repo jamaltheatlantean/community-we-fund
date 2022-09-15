3fbf6bf855700e3d7de6602f9ce86a8a5e0a3e1b
Jamal The Atlantean <jamaltheatlantean@gmail.com>
2022-09-15 22:21:34 +0100

    Update WeFund.sol

--- contracts/WeFund.sol
+++ contracts/WeFund.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.12;
 // Imports
 import "./PriceConverter.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
-// Error code
+// Errors
 error WeFund__NotBenefactorError();
 
 // Interfaces, Libraries, contract
