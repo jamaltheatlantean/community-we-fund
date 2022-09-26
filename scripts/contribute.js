af24e7bb4d521962c79c7f12531641ac3fd56844
Jamal The Atlantean <jamaltheatlantean@gmail.com>
2022-09-26 23:09:02 +0100

    Update contribute.js

--- scripts/contribute.js
+++ scripts/contribute.js
@@ -2,7 +2,7 @@ const { getNamedAccounts, ethers } = require("hardhat")
 
 async function main() {
     const { deployer } = await getNamedAccounts()
-    const weFund = await ethers.getContractAt("WeFund", deployer)
+    const weFund = await ethers.getContract("WeFund", deployer)
     console.log("funding contract...")
     const transactionResponse = await weFund.contribute({
         value: ethers.utils.parseEther("1"),
